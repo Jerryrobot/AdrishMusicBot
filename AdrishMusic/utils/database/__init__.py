552aca4bf1627e45e0268e165ed6130aab58bdcc
MrAdrish69 <107672386+MrAdrish69@users.noreply.github.com>
2023-03-14 00:42:48 +0530

    Add files via upload

---
+++ AdrishMusic/utils/database/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2023-2024 by MrAdrish69@Github, < https://github.com/MrAdrish69 >.
+#
+# This file is part of < https://github.com/MrAdrish69/AdrishMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MrAdrish69/AdrishMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .assistantdatabase import *
+from .memorydatabase import *
+from .mongodatabase import *
