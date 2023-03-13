e7b236b01367e8e781730d681376366f0b40552f
MrAdrish69 <107672386+MrAdrish69@users.noreply.github.com>
2023-03-13 21:02:40 +0530

    Add files via upload

--- AdrishMusic/inline/__init__.py
+++ AdrishMusic/inline/__init__.py
@@ -1,1 +1,16 @@
+#
+# Copyright (C) 2023-2024 by MrAdrish69@Github, < https://github.com/MrAdrish69 >.
+#
+# This file is part of < https://github.com/MrAdrish69/AdrishMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MrAdrish69/AdrishMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
+from .help import *
+from .play import *
+from .playlist import *
+from .queue import *
+from .settings import *
+from .song import *
+from .start import *
